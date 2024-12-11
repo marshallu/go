73035a9d4694e887e0a36bfddbc9a45392fe6c75
Christopher McComas <mccomas.chris@gmail.com>
2024-12-11 10:09:31 -0500

    update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="font-sans bg-gray-50 antialiased">
 	<div class="max-w-2xl px-6 mx-auto py-8">
-		<a href="/"><h1 class="text-green font-bold text-3xl">GoMarshall</h1></a>
+		<a href="/"><h1 class="text-green font-bold text-3xl">GoMarshall ---</h1></a>
 		<div class="mt-2 text-lg font-semibold text-gray-900">Create shortened URLs and QR codes for Marshall University.</div>
 	</div>
 
