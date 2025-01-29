4f15d5425ea5abf2e48cc3733b5b8cf4dab27030
Christopher McComas <mccomas.chris@gmail.com>
2025-01-29 07:55:43 -0500

    add marshallhealth to allowed domains

--- app/Rules/IsAllowedDomain.php
+++ app/Rules/IsAllowedDomain.php
@@ -10,16 +10,27 @@ class IsAllowedDomain implements ValidationRule
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-		$allowedDomains = ['www.marshall.edu', 'jcesom.marshall.edu', 'www.herdzone.com', 'www.formarshallu.org', 'dynamicforms.ngwebsolutions.com', 'federation.ngwebsolutions.com', 'livemarshall.sharepoint.com', 'www.youtube.com', 'youtube.com'];
+        $allowedDomains = [
+            'www.marshall.edu',
+            'jcesom.marshall.edu',
+            'www.herdzone.com',
+            'www.formarshallu.org',
+            'dynamicforms.ngwebsolutions.com',
+            'federation.ngwebsolutions.com',
+            'livemarshall.sharepoint.com',
+            'www.youtube.com',
+            'youtube.com',
+            'www.marshallhealth.org'
+        ];
 
-		$url = parse_url($value);
+        $url = parse_url($value);
 
-		if (!in_array($url['host'], $allowedDomains)) {
-			$fail("The :attribute must be a valid domain.");
-		}
+        if (!in_array($url['host'], $allowedDomains)) {
+            $fail("The :attribute must be a valid domain.");
+        }
     }
 }
