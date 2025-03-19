c701280ad15a0ea5557ed932b2d086f778164cc1
Christopher McComas <mccomas.chris@gmail.com>
2025-03-19 16:17:04 -0400

    testing

--- resources/views/livewire/urls/index.blade.php
+++ resources/views/livewire/urls/index.blade.php
@@ -7,7 +7,6 @@ use Livewire\WithPagination;
 use Livewire\WithoutUrlPagination;
 use Illuminate\Support\Facades\Gate;
 
-
 new class extends Component {
 	use WithPagination, WithoutUrlPagination;
 
